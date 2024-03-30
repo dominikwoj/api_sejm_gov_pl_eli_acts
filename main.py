dde6dbb8dff3cbece80ed398a7de202755138145
Dominik Wojnowski <d.wojnowski.priv@gmail.com>
2024-03-30 23:59:14 +0100

    added request to sejm

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
+from pprint import pprint
 
 def main():
+    url = 'https://api.sejm.gov.pl/eli/acts/DU/2023/1590'
+    response = requests.get(url).json()
+    fields = {'ELI': ('identyfikator_aktu', True)}
     pass
 
 if __name__ == '__main__':
